395a6a79b9daa18c1e99a484f5c4b7585d212a7d
michael mason <michael@michaeljmason.com>
2017-08-28 10:42:31 -0700

    :)

--- BasicCard.js
+++ BasicCard.js
@@ -1,24 +1,8 @@
 // Basic Flash Cards
 
-const basicQuestions = require('./basicQuestions.js');
-const inquirer = require('inquirer');
-
-function BasicCard (questions) {
-
-    this.questionList = questions;
-
+function BasicCard (front, back) {
+    this.front = front;
+    this.back = back;
 }
-
-BasicCard.prototype.askQuestions = function () {
-    inquirer.prompt(basicQuestions.questions).then( function () {
-        console.log('hey')
-    });
-}
-
-
-const basic = new BasicCard(basicQuestions);
-
-
-basic.askQuestions();
 
 module.exports = BasicCard;
